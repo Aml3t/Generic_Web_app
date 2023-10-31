3534eaadb51631bcda88be3e80122104915ef1ac
Aml3t <fischerparsos@gmail.com>
2023-10-31 14:07:44 +0200

    Adding Javascript

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -4,3 +4,12 @@ var s = "";
 alert("Hello User");
 
 var theForm = document.getElementById("theForm");
+theForm.hidden = true;
+
+var buyButton = document.getElementById("buyButton");
+buyButton.addEventListener("click", function () {
+    console.alert("Buying Item");
+})
+
+var productInfo = document.getElementsByClassName("product-props");
+var listItems = productInfo.item[0].children;
