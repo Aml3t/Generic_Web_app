678b2d772ad9f6bc5a3c4f9ac18980c78e6a722c
Aml3t <fischerparsos@gmail.com>
2023-11-01 12:32:36 +0200

    Adding controllers

---
+++ DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController :Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
