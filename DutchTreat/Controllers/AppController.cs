47d8edf445ba21f27707b554692aa41f27968fcb
Aml3t <fischerparsos@gmail.com>
2023-11-01 14:43:47 +0200

    Adding Layout

--- DutchTreat/Controllers/AppController.cs
+++ DutchTreat/Controllers/AppController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace DutchTreat.Controllers
 {
@@ -6,6 +7,7 @@ namespace DutchTreat.Controllers
     {
         public IActionResult Index()
         {
+            throw new InvalidProgramException("U Mirin Bruh?");
             return View();
         }
     }
--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -22,6 +22,11 @@ namespace DutchTreat
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             app.UseStaticFiles();
 
             app.UseRouting();
