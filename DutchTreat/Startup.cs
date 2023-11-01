6f7230753a060a95d4c512d08cfee4cd96be9ea4
Aml3t <fischerparsos@gmail.com>
2023-11-01 13:55:26 +0200

    Adding controllers

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -16,13 +16,22 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDefaultFiles();
             app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(cfg =>
+            {
+                cfg.MapControllerRoute("Default",
+                    "/{controller}/{action}/{id?}",
+                    new { controller = "App", action = "Index" });
+            });
         }
     }
 }
