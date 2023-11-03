218cf44680f8ab75ec02054b7eefaff4bbe5b803
Aml3t <fischerparsos@gmail.com>
2023-11-03 11:05:17 +0200

    Adding contact page

--- DutchTreat/Controllers/AppController.cs
+++ DutchTreat/Controllers/AppController.cs
@@ -15,8 +15,6 @@ namespace DutchTreat.Controllers
         [HttpGet("contact")]
         public IActionResult Contact()
         {
-            ViewBag.Title = "Contact Us";
-
             return View();
         }
 
