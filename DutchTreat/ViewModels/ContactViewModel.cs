6524573f10487deaa507097d8833e5291cecc142
Aml3t <fischerparsos@gmail.com>
2023-11-03 11:08:28 +0200

    Model binding

---
+++ DutchTreat/ViewModels/ContactViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DutchTreat.ViewModels
+{
+    public class ContactViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
