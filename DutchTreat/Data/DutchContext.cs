adc2cb80c4088060a929baeb23a872c1aff78243
Aml3t <fischerparsos@gmail.com>
2023-11-07 15:31:29 +0200

    Creating Entities

--- DutchTreat/Data/DutchContext.cs
+++ DutchTreat/Data/DutchContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DutchTreat.Data.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DutchTreat.Data
 {
     public class DutchContext :DbContext
     {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
 
     }
 }
