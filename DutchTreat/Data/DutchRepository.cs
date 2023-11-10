4504cf483b89f32e14ff1463165c0ffb7ccf7de7
Aml3t <fischerparsos@gmail.com>
2023-11-10 11:26:56 +0200

    Repository

--- DutchTreat/Data/DutchRepository.cs
+++ DutchTreat/Data/DutchRepository.cs
@@ -1,4 +1,9 @@
-﻿namespace DutchTreat.Data
+﻿using DutchTreat.Data.Entities;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DutchTreat.Data
 {
     public class DutchRepository
     {
@@ -7,6 +12,27 @@
         public DutchRepository(DutchContext context)
         {
             _context = context;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _context.Products
+                .OrderBy(p => p.Title)
+                .ToList();
+        }
+
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+            return _context.Products
+                .Where(p => p.Category == category)
+                .ToList();
+        }
+
+        public IEnumerable<Order> GetAllOrders()
+        {
+            return _context.Orders
+                .OrderBy(p => p.OrderDate)
+                .ToList();
         }
     }
 }
