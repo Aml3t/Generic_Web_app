c30c46c32a58127a940c4736047180ea8d3c73f2
Aml3t <fischerparsos@gmail.com>
2023-11-10 11:16:38 +0200

    Repository

---
+++ DutchTreat/Data/DutchRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace DutchTreat.Data
+{
+    public class DutchRepository
+    {
+        private readonly DutchContext _context;
+
+        public DutchRepository(DutchContext context)
+        {
+            _context = context;
+        }
+    }
+}
