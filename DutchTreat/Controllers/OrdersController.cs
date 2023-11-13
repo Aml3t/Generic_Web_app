100931f2c69d5eedd92fc01d3b3796d7e3a9239a
Aml3t <fischerparsos@gmail.com>
2023-11-13 14:12:53 +0200

    API returning data

--- DutchTreat/Controllers/OrdersController.cs
+++ DutchTreat/Controllers/OrdersController.cs
@@ -1,6 +1,34 @@
-﻿namespace DutchTreat.Controllers
+﻿using DutchTreat.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace DutchTreat.Controllers
 {
-    public class OrdersController
+    [Route("api/[Controller]")]
+    public class OrdersController : Controller
     {
+        private readonly IDutchRepository _repository;
+        private readonly ILogger _logger;
+
+        public OrdersController(IDutchRepository repository, ILogger logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return _repository.GetAllProducts();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to get orders {ex}");
+                return BadRequest("Failed to get orders");
+            }
+        }
     }
 }
