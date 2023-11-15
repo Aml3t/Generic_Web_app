9ee23a6e04f63cc88db515a0873060fc8db014e4
Aml3t <fischerparsos@gmail.com>
2023-11-15 14:23:45 +0200

    Validation and View Models

---
+++ DutchTreat/ViewModels/OrderViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DutchTreat.Data.Entities;
+using System.Collections.Generic;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DutchTreat.ViewModels
+{
+    public class OrderViewModel
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        
+        [Required]
+        public string OrderNumber { get; set; }
+
+    }
+}
