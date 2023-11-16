02407b7fc2c1f9f8ff1ad0521276696273c74cd0
Aml3t <fischerparsos@gmail.com>
2023-11-16 15:37:29 +0200

    Creating association controllers

--- DutchTreat/Data/DutchMappingProfile.cs
+++ DutchTreat/Data/DutchMappingProfile.cs
@@ -12,6 +12,9 @@ namespace DutchTreat.Data
             CreateMap<Order, OrderViewModel>()
                 .ForMember(o => o.OrderId, ex => ex.MapFrom(o => o.Id))
                 .ReverseMap();
+
+            CreateMap<OrderItem, OrderItemViewModel>()
+                .ReverseMap();
         }
     }
 }
--- DutchTreat/ViewModels/OrderItemViewModel.cs
+++ DutchTreat/ViewModels/OrderItemViewModel.cs
@@ -1,6 +1,16 @@
-﻿namespace DutchTreat.ViewModels
+﻿using DutchTreat.Data.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace DutchTreat.ViewModels
 {
     public class OrderItemViewModel
     {
+        public int Id { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public decimal UnitPrice { get; set; }
     }
 }
