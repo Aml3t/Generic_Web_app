f2cb50c9b95eb4b1333f279b40530c17e1f5ed9f
Aml3t <fischerparsos@gmail.com>
2023-11-21 10:59:38 +0200

    Creating association controllers

--- DutchTreat/Controllers/OrderItemsController.cs
+++ DutchTreat/Controllers/OrderItemsController.cs
@@ -1,14 +1,24 @@
-﻿using DutchTreat.Data;
+﻿using AutoMapper;
+using DutchTreat.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace DutchTreat.Controllers
 {
-    public class OrderItemsController
+    [Route ("/api/orders/{orderid}/items")]
+    public class OrderItemsController : Controller
     {
+        private readonly IDutchRepository _repository;
+        private readonly ILogger<OrderItemsController> _logger;
+        private readonly IMapper _mapper;
+
         public OrderItemsController(IDutchRepository repository,
-            ILogger<OrderItemsController> logger)
+            ILogger<OrderItemsController> logger,
+            IMapper mapper)
         {
-                
+            _repository = repository;
+            _logger = logger;
+            _mapper = mapper;
         }
     }
 }
