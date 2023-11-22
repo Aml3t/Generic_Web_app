cf7917b78e3f431c39fc9f894ddbdaf2644f818e
Aml3t <fischerparsos@gmail.com>
2023-11-22 14:42:48 +0200

    Login View

---
+++ DutchTreat/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DutchTreat.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+       
+        [Required]
+        public string Password{ get; set; }
+
+        public bool RememberMe { get; set; }
+
+    }
+}
