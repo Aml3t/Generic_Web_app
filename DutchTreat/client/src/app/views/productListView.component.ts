8d2edf6e79d82d19c5e623fd468583aa22e78c27
Aml3t <fischerparsos@gmail.com>
2023-12-18 14:48:14 +0200

    Adding Angular component

---
+++ DutchTreat/client/src/app/views/productListView.component.ts
@@ -0,0 +1,11 @@
+
+export default class ProductListView {
+    public products = [{
+        title: "Van Gogh Mug",
+        price: "19.99"
+    },
+    {
+        title: "Van Gosh Poster",
+        price: "29.99"
+    }];
+}
