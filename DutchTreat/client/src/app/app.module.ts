e03fb080f4732b061d22e973ae50c23c69f6cdaf
Aml3t <fischerparsos@gmail.com>
2023-12-18 14:50:12 +0200

    Adding Angular component

--- DutchTreat/client/src/app/app.module.ts
+++ DutchTreat/client/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import ProductListView from './views/productListView.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+        AppComponent,
+        ProductListView
   ],
   imports: [
     BrowserModule
