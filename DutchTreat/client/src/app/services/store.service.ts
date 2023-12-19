199e62cdc6c2fe70e4b231a3ef62dc33948dfaac
Aml3t <fischerparsos@gmail.com>
2023-12-19 14:55:54 +0200

    Creating a Service

---
+++ DutchTreat/client/src/app/services/store.service.ts
@@ -0,0 +1,10 @@
+export class Store {
+    public products = [{
+        title: "Van Gogh Mug",
+        price: "19.99"
+    },
+    {
+        title: "Van Gosh Poster",
+        price: "29.99"
+    }];
+}
--- DutchTreat/client/src/app/views/productListView.component.ts
+++ DutchTreat/client/src/app/views/productListView.component.ts
@@ -5,12 +5,5 @@ import { Component } from "@angular/core";
     templateUrl: "productListView.component.html"
 })
 export default class ProductListView {
-    public products = [{
-        title: "Van Gogh Mug",
-        price: "19.99"
-    },
-    {
-        title: "Van Gosh Poster",
-        price: "29.99"
-    }];
+
 }
