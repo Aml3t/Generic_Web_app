35ea31bdfb7fc1612565d81c38d5794c56bd3a57
Aml3t <fischerparsos@gmail.com>
2023-12-19 15:00:14 +0200

    Creating a Service

--- DutchTreat/client/src/app/views/productListView.component.ts
+++ DutchTreat/client/src/app/views/productListView.component.ts
@@ -1,9 +1,12 @@
 import { Component } from "@angular/core";
+import { Store } from "../services/store.service";
 
 @Component({
     selector: "product-list",
     templateUrl: "productListView.component.html"
 })
 export default class ProductListView {
+    constructor(private store: Store) {
 
+    }
 }
