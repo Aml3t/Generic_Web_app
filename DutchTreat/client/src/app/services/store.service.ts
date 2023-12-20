4478fff31e5624e58a26918e97aee11d9b0f913d
Aml3t <fischerparsos@gmail.com>
2023-12-20 11:53:17 +0200

    Calling the API

--- DutchTreat/client/src/app/services/store.service.ts
+++ DutchTreat/client/src/app/services/store.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { map } from "rxjs/operators";
 
 @Injectable()
 
@@ -11,6 +12,13 @@ export class Store {
 
     public products = [];
 
+    loadProducts() {
+        return this.http.get("/api/products")
+            .pipe(map(data => {
+                this.products = data;
+                return;
+            } ));
+    }
 //    {
 //    title: "Van Gogh Mug",
 //        price: "19.99"
