ecfdc98ed3c5b418377dec357bb2ecf30b5e2f6d
Aml3t <fischerparsos@gmail.com>
2023-12-20 12:25:30 +0200

    Use type safety

---
+++ DutchTreat/client/src/app/shared/Product.ts
@@ -0,0 +1,15 @@
+
+export class Product {
+    id: number
+    category: string
+    size: string
+    price: number
+    title: string
+    artDescription: string
+    artDating: string
+    artId: string
+    artist: string
+    artistBirthDate: string
+    artistDeathDate: string
+    artistNationality: string
+} []
