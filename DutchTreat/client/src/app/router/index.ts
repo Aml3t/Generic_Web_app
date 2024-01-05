b884e9f312f0db8635f7af3f4356c62d5a0e56ea
Aml3t <fischerparsos@gmail.com>
2024-01-05 13:57:44 +0200

    Adding Routing to the project

--- DutchTreat/client/src/app/router/index.ts
+++ DutchTreat/client/src/app/router/index.ts
@@ -1,10 +1,15 @@
+import { RouterModule } from "@angular/router";
+import { CheckoutPage } from "../pages/checkout.component";
 import { ShopPage } from "../pages/shopPage.component";
+
 
 
 const routes = [
     { path: "", component: ShopPage },
-    { }
+    { path: "checkout", component: CheckoutPage }
+
 ];
 
+const router = RouterModule.forRoot(routes);
 
 export default router;
