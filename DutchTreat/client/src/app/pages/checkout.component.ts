2fa05e8e6ed1459e6bd4cd1104c72620a298aea3
Aml3t <fischerparsos@gmail.com>
2024-01-05 13:07:39 +0200

    Adding Routing to the project

---
+++ DutchTreat/client/src/app/pages/checkout.component.ts
@@ -0,0 +1,26 @@
+﻿import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { Store } from "../services/store.service";
+
+@Component({
+  selector: "checkout",
+  templateUrl: "checkout.component.html",
+  styleUrls: ['checkout.component.css']
+})
+export class CheckoutPage {
+
+  public errorMessage = "";
+
+  constructor(public store: Store, private router: Router) {
+  }
+
+  onCheckout() {
+    this.errorMessage = "";
+    this.store.checkout()
+      .subscribe(() => {
+        this.router.navigate(["/"]);
+      }, err => {
+          this.errorMessage = `Failed to checkout: ${err}`;
+      })
+  }
+}
