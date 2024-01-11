23248836eb61f0ef51e43ac9748274b769a709ec
Aml3t <fischerparsos@gmail.com>
2024-01-11 14:01:05 +0200

    Support Login

--- DutchTreat/client/src/app/services/authActivator.service.ts
+++ DutchTreat/client/src/app/services/authActivator.service.ts
@@ -1,6 +1,27 @@
 import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { Observable } from "rxjs";
+import { Store } from "./store.service";
 
 @Injectable()
-export class AuthActivator {
+export class AuthActivator implements CanActivate {
+
+
+    constructor(private store: Store) {
+
+    }
+
+    canActivate(route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot)
+        : boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+
+        if (this.store.loginRequired) {
+            return false;
+        }
+        else {
+            return true;
+        }
+
+    }
 
 }
