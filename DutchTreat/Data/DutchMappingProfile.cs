a31342e69bf681fa148b53cc241b15475c91cdb7
Aml3t <fischerparsos@gmail.com>
2024-01-15 13:32:56 +0200

    Token authentication

--- DutchTreat/Data/DutchMappingProfile.cs
+++ DutchTreat/Data/DutchMappingProfile.cs
@@ -14,7 +14,8 @@ namespace DutchTreat.Data
                 .ReverseMap();
 
             CreateMap<OrderItem, OrderItemViewModel>()
-                .ReverseMap();
+                .ReverseMap()
+                .ForMember(m => m.Product, opt => opt.Ignore());
         }
     }
 }
