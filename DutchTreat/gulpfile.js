b6d05feeb55648f7838b139ef3a6d818bc9deace
Aml3t <fischerparsos@gmail.com>
2024-01-16 12:07:56 +0200

    Minifying Javascript

--- DutchTreat/gulpfile.js
+++ DutchTreat/gulpfile.js
@@ -1,9 +1,16 @@
-
 var gulp = require("gulp");
+var unglify = require("gulp-unglify");
+var concat = ("gulp-concat");
 
 // minify Javascript
 
 function minify() {
     return gulp.src(["wwwroot/js/**/*.js"])
+        .pipe(unglify())
+        .pipe(concat("dutchtreat.min.js"))
         .pipe(gulp.dest("wwwroot/dist/"))
 }
+
+// minify CSS
+
+export.default = minify();
